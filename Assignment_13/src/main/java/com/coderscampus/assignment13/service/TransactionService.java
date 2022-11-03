49cdd5083936959e311f87535020105106ef4068
Joseph Kirkish <kirkish.2@gmail.com>
2022-11-02 20:29:55 -0400

    updates
    updates

--- Assignment_13/src/main/java/com/coderscampus/assignment13/service/TransactionService.java
+++ Assignment_13/src/main/java/com/coderscampus/assignment13/service/TransactionService.java
@@ -1,10 +1,24 @@
 package com.coderscampus.assignment13.service;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.coderscampus.assignment13.domain.Transaction;
+import com.coderscampus.assignment13.domain.User;
+import com.coderscampus.assignment13.repository.TransactionRepository;
+import com.coderscampus.assignment13.repository.UserRepository;
 
 @Service
 public class TransactionService {
 	
+	@Autowired
+	private TransactionRepository transRepo;
+	
+	public Set<Transaction> findAll () {
+		return (Set<Transaction>) transRepo.findAll();
+	}
 	
 
 }
--- Assignment_13/src/main/java/com/coderscampus/assignment13/web/TransactionController.java
+++ Assignment_13/src/main/java/com/coderscampus/assignment13/web/TransactionController.java
@@ -1,8 +1,14 @@
 package com.coderscampus.assignment13.web;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.coderscampus.assignment13.domain.Transaction;
+import com.coderscampus.assignment13.domain.User;
 import com.coderscampus.assignment13.service.TransactionService;
 import com.coderscampus.assignment13.service.UserService;
 
@@ -12,6 +18,27 @@ public class TransactionController {
 	@Autowired
 	private TransactionService transactionService;
 	
+	@GetMapping("/checking")
+	public String getCheckingTransactions (ModelMap model) {
+		Set<Transaction> transactions = transactionService.findAll();
+		
+		model.put("transactions", transactions);
+		if (transactions.size() == 1) {
+			model.put("transaction", transactions.iterator().next());
+		}
+		
+		return "checking";
+	}
 	
-
+	@GetMapping("/saving")
+	public String getSavingTransactions (ModelMap model) {
+		Set<Transaction> transactions = transactionService.findAll();
+		
+		model.put("transactions", transactions);
+		if (transactions.size() == 1) {
+			model.put("transaction", transactions.iterator().next());
+		}
+		
+		return "saving";
+	}
 }
